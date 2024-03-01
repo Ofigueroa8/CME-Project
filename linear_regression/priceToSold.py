e42509886fe510a99598e47bdc2483bb1fde5f51
Omar Figueroa <ofigueroa8@cps.edu>
2024-03-01 10:39:55 -0600

    Testing visual representation

--- linear_regression/priceToSold.py
+++ linear_regression/priceToSold.py
@@ -9,8 +9,6 @@ data = pd.read_csv("linear_regression/datesFfrHousesPrices.csv")
 x = data[["housesSold","fedfunds"]]
 y = data["lumberPrice"]
 
-print(x)
-
 pprint.pprint(data)
 data["dates"] = pd.to_datetime(data["dates"])
 
@@ -25,50 +23,58 @@ model = LinearRegression().fit(xtrain, ytrain)
 # Find the coefficient, bias, and r squared values. 
 # Each should be a float and rounded to two decimal places. 
 print(model.coef_)
-coef = np.round(float(model.coef_), 2)
+coef = np.round(model.coef_, 2)
 intercept = round(float(model.intercept_), 2)
-r_squared = model.score(xtrain, ytrain)
+r_squared = round(model.score(x, y),2)
 
 # Print out the linear equation and r squared value:
-print("Model's Linear Equation: y=",coef, "x+", intercept)
+print(f"Model's Linear Equation: y= {coef[0]}x1 + {coef[1]}x2 + {intercept}")
 print("R Squared value:", r_squared)
 
 '''
 **********TEST THE MODEL**********
 '''
-# reshape the xtest data into a 2D array
-xtest = xtest.reshape(-1,1)
 
 # get the predicted y values for the xtest values - returns an array of the results
 predict = model.predict(xtest)
 # round the value in the np array to 2 decimal places
 predict = np.around(predict, 2)
+print(predict)
 
 # Test the model by looping through all of the values in the xtest dataset
 print("\nTesting Linear Model with Testing Data:")
 
-for index in range(len(xtest)):
-    actual = ytest[index] # gets the actual y value from the ytest dataset
-    predicted_y = predict[index] # gets the predicted y value from the predict variable
-    x_coord = xtest[index] # gets the x value from the xtest dataset
-    print("x value:", float(x_coord), "Predicted y value:", predicted_y, "Actual y value:", actual)
 
-'''
-**********CREATE A VISUAL OF THE RESULTS**********
-'''
-#sets the size of the graph
-plt.figure(figsize=(5,4))
+print(type(ytest))
+print(ytest)
+print(type(xtest))
+print(xtest)
 
-#creates a scatter plot and labels the axes
-plt.scatter(xtrain,ytrain, c="purple", label="Training Data")
-plt.scatter(xtest, ytest, c="blue", label="Testing Data")
 
-plt.scatter(xtest, predict, c="red", label="Predictions")
+# for index in range(len(xtest)):
+#     actual = ytest[index] # gets the actual y value from the ytest dataset
+    # predicted_y = predict[index] # gets the predicted y value from the predict variable
+    # x_coord = xtest[index] # gets the x value from the xtest dataset
+#     print(f"Houses Sold: {x_coord[0]} Fedfunds Rate: {x_coord[1]} Actual: {actual} Predicted: {predicted_y}")
+    
 
-plt.xlabel("Lumber")
-plt.ylabel("Houses Sold")
-plt.title("Houses Sold Based on Lumber Prices")
-plt.plot(x, coef*x + intercept, c="r", label="Line of Best Fit")
 
-plt.legend()
-plt.show()
+# '''
+# **********CREATE A VISUAL OF THE RESULTS**********
+# '''
+# #sets the size of the graph
+# plt.figure(figsize=(5,4))
+
+# #creates a scatter plot and labels the axes
+# plt.scatter(xtrain,ytrain, c="purple", label="Training Data")
+# plt.scatter(xtest, ytest, c="blue", label="Testing Data")
+
+# plt.scatter(xtest, predict, c="red", label="Predictions")
+
+# plt.xlabel("Lumber")
+# plt.ylabel("Houses Sold")
+# plt.title("Houses Sold Based on Lumber Prices")
+# plt.plot(x, coef*x + intercept, c="r", label="Line of Best Fit")
+
+# plt.legend()
+# plt.show()
